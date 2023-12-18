92c72a020dd33c89fdf28ee08b6eeaab7421a696
Pazl <jaul.ponas@gmail.com>
2023-12-18 13:41:54 +0100

    boilerplate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,12 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Des is mein Branch");
         System.out.println("Ig will nimmer");
+
+        // Beispielaufruf der train-Methode mit zufälligen Daten.
+        NeuronalNetworkTraining trainer = new NeuronalNetworkTraining();
+        double[][] inputPatterns = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        double[] outputPatterns = {0, 1, 1, 0};
+
+        trainer.train(inputPatterns, outputPatterns);
     }
 }
