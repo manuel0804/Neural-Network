666c94a9aa652dbff087970d4918bac9b7a8bbd1
22alyu1bif <22alyu1bif@hft-stuttgart.de>
2023-12-20 10:45:55 +0100

    Finishes CSVReader

--- src/main/java/org/example/CSVReader.java
+++ src/main/java/org/example/CSVReader.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class CSVReader {
     static List<double[]> dataList = new ArrayList<>();
-    public static void main(String[] args) {
+    public void loadCSV() {
         // Pfad zur CSV-Datei
         String csvFilePath = "src/main/resources/diabetes.csv";
 
